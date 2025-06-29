5cb42b2645ea48da6a71648e38f40a1bb0925992
Lai Hong Yu <joyjay606@gmail.com>
2025-06-29 19:57:54 +0800

    Refactor authentication in route.js to use getAuth from Clerk and update user creation logic

--- src/app/api/usersync/route.js
+++ src/app/api/usersync/route.js
@@ -1,26 +1,27 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
-  const user = await currentUser();
-  if (!user) return new Response("Unauthorized", { status: 401 });
-
-  // Check if user exists in DB
-  let dbUser = await prisma.user.findUnique({
-    where: { clerkId: user.id },
+export async function GET(request) {
+  const { userId, sessionClaims } = getAuth(request);
+    if (!userId) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    console.log("sessionClaims:", sessionClaims);
+    const email = sessionClaims?.email;
+    let dbUser = await prisma.user.findUnique({
+    where: { clerkId: userId },
   });
 
-  // If not, create with role based on email domain
   if (!dbUser) {
     const teacherDomain = "@s2024.ssts.edu.sg";
-    const role = user.emailAddresses[0].emailAddress.endsWith(teacherDomain)
+    const role = email.endsWith(teacherDomain)
       ? "teacher"
       : "student";
 
     dbUser = await prisma.user.create({
       data: {
-        clerkId: user.id,
-        email: user.emailAddresses[0].emailAddress,
+        clerkId: userId,
+        email: email,
         role,
       },
     });
