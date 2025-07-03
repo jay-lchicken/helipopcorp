bb6a77eecf8b547bcfa6135868e21a48209735dd
Helipop <117784321+xrh999@users.noreply.github.com>
2025-07-03 14:54:24 +0800

    Update GetAssignments

--- src/app/api/GetAssignments/route.js
+++ src/app/api/GetAssignments/route.js
@@ -1,22 +1,31 @@
-import pool from "@/lib/db";
-import {getAuth} from "@clerk/nextjs/server";
-import {NextResponse} from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { NextResponse } from "next/server";
 import * as crypto from "node:crypto";
-export async function GET(res) {
-    const { userId, sessionClaims } = getAuth(res);
-    if (!userId) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-    const email = sessionClaims?.email;
-    const hash_userid_email = crypto.createHash("sha256").update(email + userId).digest("hex");
-    try {
+import pool from "@/lib/db";
+
+export async function GET(req) {
+  const { userId, sessionClaims } = getAuth(req);
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const email = sessionClaims?.email;
+  const hash_userid_email = crypto.createHash("sha256").update(email + userId).digest("hex");
+  try {
+    const user = await clerkClient.users.getUser(userId);
+    const role = user.privateMetadata?.isAdmin;
+    if (!role) {
         const assignments = await pool.query(
             "SELECT * FROM assignments WHERE hash_userid_email = $1",
             [hash_userid_email]
         );
         return NextResponse.json(assignments.rows);
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    } else {
+      const assignments = await pool.query("SELECT * FROM assignments");
+      return NextResponse.json(assignments.rows);
     }
+  } catch (error) {
+    console.error("Error checking admin:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
