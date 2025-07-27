dbad9bfc45baabd253b3d630db7c06b3f977bd2a
Lai Hong Yu <joyjay606@gmail.com>
2025-07-27 18:05:41 +0800

    Refactor user data fetching and enhance query for completed assignments

--- src/app/student-dashboard/page.js
+++ src/app/student-dashboard/page.js
@@ -8,9 +8,9 @@ export default async function Main() {
   const { userId, sessionClaims } = await auth()
     const email = sessionClaims?.email;
 
-    // var user;
-    // var clerkUser;
-    // console.log("USER ID" + userId)
+    var user;
+    var clerkUser;
+    console.log("USER ID" + userId)
 
 
 
@@ -26,13 +26,13 @@ export default async function Main() {
             credentials: 'include'
         });
         if (res.ok) {
-            user = await res.json();
+             user = await res.json();
         }
     } catch (err) {
         console.error("Failed to fetch user from DB", err);
     }
     console.log("Email:", email);
-    const userResult = await pool.query('SELECT * FROM submissions JOIN assignments a ON submissions.assignment_id = a.id WHERE submissions.name = $1', [email]);
+    const userResult = await pool.query('SELECT *, a.name as assignment_name FROM submissions JOIN assignments a ON submissions.assignment_id = a.id WHERE submissions.name = $1;', [email]);
     return (
         <CompletedAssignments
             submissions={userResult.rows}
